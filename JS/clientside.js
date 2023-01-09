6baa04da1f1573eb4abdddc1ddffe99336522ee1
Rajat002 <rajatbassi002@gmail.com>
2023-01-09 21:11:26 +0530

    feature addition

--- JS/clientside.js
+++ JS/clientside.js
@@ -4,12 +4,18 @@ const form= document.getElementById('send-container');
 const messageInput=document.getElementById('messageInp');
 const messagecontainer=document.querySelector(".container");
 
+var audio= new Audio('sound.mp3');
+
+
 const append=(message,position)=>{
     const messageElement=document.createElement('div');
     messageElement.innerText=message;
     messageElement.classList.add('message');
     messageElement.classList.add(position);
     messagecontainer.append(messageElement);
+    if(position==left){
+    audio.play();
+    }
 }
 
 form.addEventListener('submit',(e)=>{
@@ -29,3 +35,6 @@ socket.on('user-joined',nameof=>{
 socket.on('receive',data=>{
     append(`${data.nameof}: ${data.message}`,'left')
  })
+ socket.on('left',nameof=>{
+  append(`${nameof} left the chat`,'left')
+})
